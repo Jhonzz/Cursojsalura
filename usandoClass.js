4bdcaafb47db76900a539066ddb68ed5712488c8
Jhon <96670392+Jhonzz@users.noreply.github.com>
2022-05-24 16:38:52 -0300

    2

---
+++ usandoClass.js
@@ -0,0 +1,28 @@
+console.log('Usando o "Class"\n ');
+
+class usuario {
+  usuario;
+  email;
+  idade;
+}
+
+class contaCorrente(
+  agencia
+  saldo
+
+)
+
+const usuario1 = new usuario();
+const usuario2 = new usuario();
+
+usuario1.usuario = "Jhonzz";
+usuario1.email = "jhonpablo123@gmail.com";
+usuario1.idade = 16;
+
+usuario2.usuario = "Anazz";
+usuario2.email = "ana123@hotmail.com";
+usuario2.idade = 20;
+
+console.log(usuario1);
+console.log(usuario2);
+
---
+++ usandoclass1.js
@@ -0,0 +1,10 @@
+console.log("Usando classes\n");
+class exemplo {
+  exemplo;
+}
+
+const cliente1 = new exemplo();
+
+cliente1.exemplo = "Jhon";
+
+console.log(cliente1);
